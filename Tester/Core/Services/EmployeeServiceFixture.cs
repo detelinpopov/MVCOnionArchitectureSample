0e8184a44b1e8e0157adbbe68bbf8a8aab15b2d7
detelinpopov <detelin.popov@pariplaydev.com>
2017-07-10 13:34:22 +0300

    ReSharper adjustments

--- Tester/Core/Services/EmployeeServiceFixture.cs
+++ Tester/Core/Services/EmployeeServiceFixture.cs
@@ -1,92 +1,87 @@
 ﻿using System.Threading.Tasks;
-
 using Core.Services;
-
 using Interfaces.Sql.Entities;
-
 using Moq;
-
 using NUnit.Framework;
-
 using Sql.Entities;
 using Sql.Repositories;
 
 namespace Tester.Core.Services
 {
-	[TestFixture]
-	public class EmployeeServiceFixture
-	{
-		private Mock<EmployeeRepository> _repositoryMock;
+    [TestFixture]
+    public class EmployeeServiceFixture
+    {
+        private Mock<EmployeeRepository> _repositoryMock;
 
-		private void SetupRepositoryMock()
-		{
-			IEmployee employee = CreateEmployee();
-			_repositoryMock = new Mock<EmployeeRepository>();
-			_repositoryMock.Setup(r => r.SaveAsync(It.IsAny<IEmployee>())).Returns(Task.CompletedTask);
-			_repositoryMock.Setup(r => r.FindAsync(It.IsAny<int>())).ReturnsAsync(employee);
-		}
+        private void SetupRepositoryMock()
+        {
+            IEmployee employee = CreateEmployee();
+            _repositoryMock = new Mock<EmployeeRepository>();
+            _repositoryMock.Setup(r => r.SaveAsync(It.IsAny<IEmployee>())).Returns(Task.CompletedTask);
+            _repositoryMock.Setup(r => r.FindAsync(It.IsAny<int>())).ReturnsAsync(employee);
+        }
 
-		private IEmployee CreateEmployee(int index = 1)
-		{
-			IEmployee employee = new Employee();
-			employee.Name = "TestName " + index;
-			employee.Email = "test@test.com";
-			employee.ManagerId = index;
-			return employee;
-		}
+        private IEmployee CreateEmployee(int index = 1)
+        {
+            IEmployee employee = new Employee();
+            employee.Name = "TestName " + index;
+            employee.Email = "test@test.com";
+            employee.ManagerId = index;
+            return employee;
+        }
 
-		private bool VerifyEmployee(IEmployee original, IEmployee toCompare)
-		{
-			Assert.AreEqual(original.Name, toCompare.Name);
-			Assert.AreEqual(original.Email, toCompare.Email);
-			Assert.AreEqual(original.ManagerId, toCompare.ManagerId);
+        private bool VerifyEmployee(IEmployee original, IEmployee toCompare)
+        {
+            Assert.AreEqual(original.Name, toCompare.Name);
+            Assert.AreEqual(original.Email, toCompare.Email);
+            Assert.AreEqual(original.ManagerId, toCompare.ManagerId);
 
-			return true;
-		}
+            return true;
+        }
 
-		[Test]
-		public async Task FindAsync_CallsRepository()
-		{
-			// Arrange
-			SetupRepositoryMock();
-		    EmployeeService service = new EmployeeService(_repositoryMock.Object);
+        [Test]
+        public async Task FindAsync_CallsRepository()
+        {
+            // Arrange
+            SetupRepositoryMock();
+            EmployeeService service = new EmployeeService(_repositoryMock.Object);
 
             // Act
             await service.FindAsync(1);
 
-			// Assert
-			_repositoryMock.Verify(r => r.FindAsync(It.IsAny<int>()), Times.Once);
-		}
+            // Assert
+            _repositoryMock.Verify(r => r.FindAsync(It.IsAny<int>()), Times.Once);
+        }
 
-		[Test]
-		public async Task SaveAsync_CallsRepository()
-		{
-			// Arrange
-			IEmployee employee = CreateEmployee();
-			SetupRepositoryMock();
+        [Test]
+        public async Task SaveAsync_CallsRepository()
+        {
+            // Arrange
+            IEmployee employee = CreateEmployee();
+            SetupRepositoryMock();
 
-		    EmployeeService service = new EmployeeService(_repositoryMock.Object);
+            EmployeeService service = new EmployeeService(_repositoryMock.Object);
 
             // Act
             await service.SaveAsync(employee);
 
-			// Assert
-			_repositoryMock.Verify(r => r.SaveAsync(It.IsAny<IEmployee>()), Times.Once);
-		}
+            // Assert
+            _repositoryMock.Verify(r => r.SaveAsync(It.IsAny<IEmployee>()), Times.Once);
+        }
 
-		[Test]
-		public async Task SaveAsync_SendsCorrectObjectToRepository()
-		{
-			// Arrange
-			IEmployee employee = CreateEmployee();
-			SetupRepositoryMock();
+        [Test]
+        public async Task SaveAsync_SendsCorrectObjectToRepository()
+        {
+            // Arrange
+            IEmployee employee = CreateEmployee();
+            SetupRepositoryMock();
 
-			// Act
-			EmployeeService service = new EmployeeService(_repositoryMock.Object);
-			await service.SaveAsync(employee);
+            // Act
+            EmployeeService service = new EmployeeService(_repositoryMock.Object);
+            await service.SaveAsync(employee);
 
-			// Assert
-			_repositoryMock.Verify(r => r.SaveAsync(It.Is<IEmployee>(e => VerifyEmployee(e, employee))), Times.Once);
-		}
-	}
+            // Assert
+            _repositoryMock.Verify(r => r.SaveAsync(It.Is<IEmployee>(e => VerifyEmployee(e, employee))), Times.Once);
+        }
+    }
 }
