e3911ffdba76fffc04ace20110a193a9b0b2cf95
detelinpopov <detelin.popov@pariplaydev.com>
2017-07-10 13:28:38 +0300

    Updated EmployeeRepository

--- Sql/Repositories/EmployeeRepository.cs
+++ Sql/Repositories/EmployeeRepository.cs
@@ -1,40 +1,42 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Threading.Tasks;
-
 using Interfaces.Sql.Context;
 using Interfaces.Sql.Entities;
 using Interfaces.Sql.Repositories;
-
 using Sql.Context;
 using Sql.Entities;
 
 namespace Sql.Repositories
 {
-	public class EmployeeRepository : IEmployeeRepository
-	{
-		private readonly CodingExerciseContext _context;
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CodingExerciseContext _context;
 
-		public EmployeeRepository()
-		{}
+        public EmployeeRepository()
+        {
+        }
 
-		public EmployeeRepository(ICodingExerciseContext context)
-		{
-			_context = context as CodingExerciseContext;
-		}
+        public EmployeeRepository(ICodingExerciseContext context)
+        {
+            _context = context as CodingExerciseContext;
+        }
 
-		public virtual async Task SaveAsync(IEmployee employee)
-		{
-			Employee employeeAsEntity = employee as Employee;
-		    if (employeeAsEntity != null)
-		    {
-		        _context.Employees.Add(employeeAsEntity);
-		        await _context.SaveChangesAsync();
-		    }
-		}
+        public virtual async Task SaveAsync(IEmployee employee)
+        {
+            Employee employeeAsEntity = employee as Employee;
+            if (employeeAsEntity == null)
+            {
+                throw new ArgumentException("Invalid Employee.");
+            }
 
-		public virtual async Task<IEmployee> FindAsync(int id)
-		{
-			return await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
-		}
-	}
+            _context.Employees.Add(employeeAsEntity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<IEmployee> FindAsync(int id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
 }
